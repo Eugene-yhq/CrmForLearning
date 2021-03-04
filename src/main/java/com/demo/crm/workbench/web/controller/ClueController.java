8d0261ada9dbdca3bc2853cfdb36dabf5454423f
yhq_e <1161020039@qq.com>
2021-03-04 22:08:03 +0800

    core constructing

--- src/main/java/com/demo/crm/workbench/web/controller/ClueController.java
+++ src/main/java/com/demo/crm/workbench/web/controller/ClueController.java
@@ -46,7 +46,26 @@ public class ClueController extends HttpServlet {
 
             detail(request, response);
 
+        }else if ("/workbench/clue/getActivityListByClueId.do".equals(path)) {
+
+            getActivityListByClueId(request, response);
+
         }
+
+
+    }
+
+    private void getActivityListByClueId(HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println("根据线索的id查询关联的市场活动列表");
+
+        String clueId = request.getParameter("clueId");
+
+        ActivityService as = (ActivityService) ServiceFactory.getService(new ActivityServiceImpl());
+
+        List<Activity> aList = as.getActivityListByClueId(clueId);
+
+        PrintJson.printJsonObj(response, aList);
 
 
     }
